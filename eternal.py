a83171be67e4319b8072f265f57cd3746c451c84
Nizel <63397699+sudo-do@users.noreply.github.com>
2021-05-17 04:22:51 +0300

    update eternal.py

--- eternal.py
+++ eternal.py
@@ -1,1 +1,14 @@
 # eternal
+token=os.getenv("token")
+import discord
+import asyncio
+import json
+import time
+from inc import sorular
+
+client = commands.Bot("-")
+@client.event
+async def on_connect():
+  print(f"logged in as {client.user}")
+  
+await client.change_presence(activity=discord.Activity(type=discord.ActivityType.custom, name="-help"))
