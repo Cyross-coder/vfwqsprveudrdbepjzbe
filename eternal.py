8ae8c50a261cc2f996d224d9b86d543d170e0cd2
Nizel <63397699+sudo-do@users.noreply.github.com>
2021-05-17 04:41:01 +0300

    update eternal.py

--- eternal.py
+++ eternal.py
@@ -6,12 +6,20 @@ import asyncio
 import json
 import time
 from discord.ext import commands
-#from inc import sorular
+from inc import soru as sorular
 
 client = commands.Bot("-")
 @client.event
 async def on_connect():
   print(f"logged in as {client.user}")
-  await client.change_presence(activity=discord.Activity(type=discord.ActivityType.custom, name="-help"))
+  await client.change_presence(activity=discord.Activity(type=discord.ActivityType.Watching, name="-help"))
+
+@client.command()
+async def soru(ctx):
+  soru = sorular.soru18()
+  await ctx.send(soru)
+@soru.error
+async def soru(ctx, devami):
+        print(f'Hassikome! {ctx.author} | {devami}')
 
 client.run(token)
