1238150919cecd036323c06c452a59b8024b86b7
Nizel <63397699+sudo-do@users.noreply.github.com>
2021-05-21 14:19:24 +0300

    update eternal.py

--- eternal.py
+++ eternal.py
@@ -202,6 +202,10 @@ class rpgame:
       maps["Binaiçi"]["specs"]["items_og"]={}
   @client.command()
   async def battle(ctx, _with: discord.Member = None, _map='random', difficulty='normal'):
+    if ctx.author.id==_with.member.id:
+      embed=discord.Embed(title="Meydan okuma başarısız", description="Kendine savaş açamazsın, baka!", color=0xff0000)
+      await ctx.send
+      return
     if await rpgame.funcs.battlereq(ctx.author.id, _with.id):
       if _map=='random':
         _map=random.choice(rpgame.things.maps.mapdict)
